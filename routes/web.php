8f8b84069f49a7394084883abec5ba0bb3f670bb
azzazhr <azzahraqina70@gmail.com>
2025-02-19 09:42:13 +0700

    add praktikum 1 (optional parameters)

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,10 @@ Route::get('/articles/{id}', function ($id) {
     return 'Halaman Artikel dengan ID ' . $id;
 });
 
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Azzahra Attaqina ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Azzahra Attaqina ' . $name;
+});
